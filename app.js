7159c474814a4b1db20cde60f45941b7e21bc357
Russ <rperry1886@gmail.com>
2020-09-08 20:48:29 -0400

    Add calculation functionality

--- app.js
+++ app.js
@@ -31,19 +31,33 @@ function addDecimal() {
   }
 }
 
+// Calculate first and second values depending on operator
+const calculate = {
+  '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+  '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+  '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+  '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+  '=': (firstNumber, secondNumber) => secondNumber,
+};
+
 function useOperator(operator) {
   const currentValue = Number(calculatorDisplay.textContent);
+  // Prevent multiple operators
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operator;
+    return;
+  }
   // Assign first value if no value
   if (!firstValue) {
     firstValue = currentValue;
   } else {
-    console.log('currentValue', currentValue);
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
   }
   // Ready for next value, store the operator
   awaitingNextValue = true;
   operatorValue = operator;
-  console.log('FirstValue', firstValue);
-  console.log('operator', operator);
 }
 
 // Add Event Lisnteners for numbers, operators, decimal buttons
